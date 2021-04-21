5f9f4bc2f5d29cab5272fc4a4dd25b677e16ad6f
Chris Chu <42784131+wanderingessence@users.noreply.github.com>
2021-04-21 12:30:31 -0400

    lunch

--- src/App.js
+++ src/App.js
@@ -2,25 +2,28 @@ import './App.css';
 import Home from './Home';
 import Header from './Header';
 import Footer from './Footer';
+import SearchPage from './SearchPage'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
 
-    //BEM
-    <div className="App">
-        {/*<h1>Hey Clever Programmer, Let's build AIRBNB clone!🚀</h1>*/}
-        {/* Home */}
-            <Header /> 
-            <Home />
-            <Footer />
-            {/*  Header  */}
-            {/*  Banner  */}
-            {/*  Cards */} 
-            {/*  Footer  */}
-        {/* SearchPage */}
-          {/* ... */}
-
-    </div>
+    <div className="app">
+    <Router>
+      <Header />
+      
+      <Switch>
+        <Route path="/search">
+          <SearchPage />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+      
+      <Footer />
+    </ Router>
+  </div>
 
   
 
