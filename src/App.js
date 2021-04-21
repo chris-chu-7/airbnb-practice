bbb23ac39d48950f3dd55ec8efdd4beee41bf118
Chris Chu <42784131+wanderingessence@users.noreply.github.com>
2021-04-21 12:05:33 -0400

    started the setup of react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './Home';
 import Header from './Header';
+import Footer from './Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         {/* Home */}
             <Header /> 
             <Home />
+            <Footer />
             {/*  Header  */}
             {/*  Banner  */}
             {/*  Cards */} 
